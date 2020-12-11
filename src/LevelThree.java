065ec5bd08656b86acc5eda09e0083f53d5705a5
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:25:33 -0700

    Update LevelThree.java

--- src/LevelThree.java
+++ src/LevelThree.java
@@ -1,4 +1,10 @@
 public class LevelThree{
+  /**
+  a class for level 3 of the game
+  @param movement from the player
+  @return movements of the player, health, dinosaurs, dinosaur health, powerups
+  TODO - add characters, level design, dinosaurs, powerups
+  */
   int difficulty = ();
   int dinosaur = 9;
   void display(powerup);
