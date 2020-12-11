ac31a8f58af850afd71feca5138dd448d4731a57
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:30:47 -0700

    Update Title.java

--- src/Title.java
+++ src/Title.java
@@ -1,4 +1,10 @@
 public class Title{
+  /**
+  a class for the title image of the game
+  @param text for title(message) and image
+  @return image and message
+  TODO - write message and make image
+  */
   public static void main(String[] args){
     String message = " ";
     void display(message);
