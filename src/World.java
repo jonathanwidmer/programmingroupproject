5ef622d8bd5ace2df4131784eacdf13c54174a07
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:32:06 -0700

    Update World.java

--- src/World.java
+++ src/World.java
@@ -1,4 +1,10 @@
 public class World{
+  /**
+  a class for the world in the g ame
+  @param movements from the player
+  @return positions of player and dinosaurs, health, status, image of player and dinosaurs
+  TODO - add player, dinosaurs, logic for health and displaying
+  */
   double score = ();
   void setDifficulty(int);
   void display(dinosaur);
