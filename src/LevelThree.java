3fa5c232d21f9566968a801914c9e0dac6fb93bc
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:15:53 -0700

    Add files via upload

---
+++ src/LevelThree.java
@@ -0,0 +1,7 @@
+public class LevelThree{
+  int difficulty = ();
+  int dinosaur = 9;
+  void display(powerup);
+  void display(dinosaur);
+  void powerup();
+}
---
+++ src/LevelTwo.java
@@ -0,0 +1,7 @@
+public class LevelTwo{
+  int difficulty = ();
+  int dinosaur = 6;
+  void display(powerup);
+  void display(dinosaur);
+  void powerup();
+}
