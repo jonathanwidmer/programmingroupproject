21c4ae90ec2f685cdc0266b9a051711e641ce0f4
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:29:37 -0700

    Update PowerUp.java

--- src/PowerUp.java
+++ src/PowerUp.java
@@ -1,4 +1,9 @@
 public class PowerUp{
+  /**
+  a class for the powerups for the player
+  @return image of powerup, name of powerup, health of player after powerup
+  TODO - add logic for powerup, add image of powerup
+  */
   String name = " ";
   public PowerUp powerup = new powerup;
   void display(powerup);
