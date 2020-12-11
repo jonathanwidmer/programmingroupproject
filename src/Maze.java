a4ff3338ecba2d8045b0ddeb5f254c071a03bc6c
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:27:25 -0700

    Update Maze.java

--- src/Maze.java
+++ src/Maze.java
@@ -1,4 +1,10 @@
 public class Maze{
+  /**
+  a class for the maze in the game
+  @param difficulty of the maze, design for the maze
+  @return image of the maze
+  TODO - add maze
+  */
   int difficulty = ();
   void printMaze();
 }
