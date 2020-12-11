de16d4bb797199b83f31f87ae60c53ef249e2b18
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:20:35 -0700

    Update Dinosaur.java

--- src/Dinosaur.java
+++ src/Dinosaur.java
@@ -1,4 +1,9 @@
 public class Dinosaur{
+  /**
+  a class for dinosaurs in the game
+  @return dinosaur species, health, displayed dinosaur
+  TODO - add health, name, image, and movement control
+  */
   public Dinosaur dinosaur = new dinosaur;
   int health = ();
   String name = " ";
