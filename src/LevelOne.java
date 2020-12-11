3201ddf69605d2e3991b26baf0d9555c9a32a944
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:23:52 -0700

    Update LevelOne.java

--- src/LevelOne.java
+++ src/LevelOne.java
@@ -1,4 +1,10 @@
 public class LevelOne{
+  /** 
+  a class for level 1 of the game
+  @param movement from the player
+  @return movements of the player, health, dinosaurs, dinosaur health, powerups
+  TODO - add characters, level design, dinosaurs, powerups
+  */
   int difficulty = ();
   int dinosaur = 3;
   void display(powerup);
