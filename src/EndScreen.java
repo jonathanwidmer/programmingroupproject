76aa350d91aee6ea2cbc3e61d4d0d8f3b831cf09
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:21:39 -0700

    Update EndScreen.java

--- src/EndScreen.java
+++ src/EndScreen.java
@@ -1,4 +1,9 @@
 public class EndScreen{
+  /**
+  a class to show the end of the game with a message to the player
+  @return ending image and message
+  TODO - add image and message
+  */
   BufferImage EndScreen();
   String ending = " ";
   void display(ending);
