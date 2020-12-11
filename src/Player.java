ba0cc33c4f971d43374df6c5d86c991268841c3f
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 13:57:10 -0700

    Add files via upload

---
+++ src/Player.java
@@ -0,0 +1,10 @@
+public class Player{
+  String name = " ";
+  public Player player = new player;
+  private int health;
+
+  public static final int ALIVE = 100;
+  public static final int WEAK = 50;
+  public static final int DYING = 20;
+  void display(player);
+}
