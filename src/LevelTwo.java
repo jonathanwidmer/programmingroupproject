a8de1acb258e852163738794b194e01f285fced1
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-11 14:26:34 -0700

    Update LevelTwo.java

--- src/LevelTwo.java
+++ src/LevelTwo.java
@@ -1,4 +1,10 @@
 public class LevelTwo{
+  /**
+  a class for level 2 of the game
+  @param movement from the player
+  @return movements of the player, health, dinosaurs, dinosaur health, powerups
+  TODO - add characters, level design, dinosaurs, powerups
+  */
   int difficulty = ();
   int dinosaur = 6;
   void display(powerup);
