8efdc02f167ec274ade5648955ae60dae31321e9
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2021-02-03 09:21:18 -0700

    Add files via upload

---
+++ src/LevelOne.java
@@ -0,0 +1,14 @@
+public class LevelOne{
+  /**
+  a class for level 3 of the game
+  @param movement from the player
+  @return movements of the player, health, dinosaurs, dinosaur health, powerups
+  TODO - add characters, level design, dinosaurs, powerups
+  */
+  int difficulty = ();
+  int dinosaur = 3;
+  void display(powerup);
+  void display(dinosaur);
+  void powerup();
+  void printMaze();
+}
