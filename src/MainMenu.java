d9a30915bc0692344114352d80d501a554764492
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2021-02-03 09:28:01 -0700

    Add files via upload

---
+++ src/MainMenu.java
@@ -0,0 +1,7 @@
+public class MainMenu{
+  public static void main(String[] args){
+    JButton play = new JButton("Play");
+    JButton timeline = new JButton("Timeline");
+    JButton resources = new JButton("Resources");
+  }
+}
