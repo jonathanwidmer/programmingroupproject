b474ce1ac3e6eeb88c1941074c00bec2ec55fde8
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2021-05-20 08:07:49 -0600

    Delete MainMenu.java

--- src/MainMenu.java
+++
@@ -1,7 +0,0 @@
-public class MainMenu{
-  public static void main(String[] args){
-    JButton play = new JButton("Play");
-    JButton timeline = new JButton("Timeline");
-    JButton resources = new JButton("Resources");
-  }
-}
